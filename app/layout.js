f7820d532262c5e50d6cc465a5b1f655ece6afef
Shubham Kumar <shubhamkumar821807@gmail.com>
2025-06-30 20:12:01 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Script from 'next/script';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased `} >
-        <script src="https://cdn.lordicon.com/lordicon.js"></script>
+       
+       <Script src="https://cdn.lordicon.com/lordicon.js" strategy="afterInteractive" />
         <div className="font-mono">
         <Navbar/>
         {children}
